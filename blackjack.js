528e44fd0cf8b749e9ffb56b40723882c5b8671b
Ian Ross <ianross@Ians-MacBook-Air.local>
2015-10-15 10:32:14 -0400

    Start of Day2 for Project1

---
+++ blackjack.js
@@ -0,0 +1,86 @@
+console.log('Works!');
+
+var deck = [
+// Diamonds
+	{"diamonds" : 2},
+	{"diamonds" : 3},
+	{"diamonds" : 4},
+	{"diamonds" : 5},
+	{"diamonds" : 6},
+	{"diamonds" : 7},
+	{"diamonds" : 8},
+	{"diamonds" : 9},
+	{"diamonds" : 10},
+	{"diamondsJack" : 10},
+	{"diamondsQueen" : 10},
+	{"diamondsKing" : 10},	
+	{"diamondsAce" : 11},
+// Clubs
+	{"clubs" : 2},
+	{"clubs" : 3},
+	{"clubs" : 4},
+	{"clubs" : 5},
+	{"clubs" : 6},
+	{"clubs" : 7},
+	{"clubs" : 8},
+	{"clubs" : 9},
+	{"clubs" : 10},
+	{"clubsJack" : 10},
+	{"clubsQueen" : 10},
+	{"clubsKing" : 10},	
+	{"clubsAce" : 11},
+// Spades
+	{"spades" : 2},
+	{"spades" : 3},
+	{"spades" : 4},
+	{"spades" : 5},
+	{"spades" : 6},
+	{"spades" : 7},
+	{"spades" : 8},
+	{"spades" : 9},
+	{"spades" : 10},
+	{"spadesJack" : 10},
+	{"spadesQueen" : 10},
+	{"spadesKing" : 10},	
+	{"spadesAce" : 11},
+// Hearts
+	{"hearts" : 2},
+	{"hearts" : 3},
+	{"hearts" : 4},
+	{"hearts" : 5},
+	{"hearts" : 6},
+	{"hearts" : 7},
+	{"hearts" : 8},
+	{"hearts" : 9},
+	{"hearts" : 10},
+	{"heartsJack" : 10},
+	{"heartsQueen" : 10},
+	{"heartsKing" : 10},	
+	{"heartsAce" : 11},
+
+
+]
+// console.log(deck);
+
+// Player and dealer arrays both start blank
+var playerArray = [];
+var dealerArray = [];
+
+var bankroll = 5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
